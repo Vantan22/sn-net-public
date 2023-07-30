cee5cb10051d388060d9ab191cf02fc68118350a
Vawntan <vawntanng@gmail.com>
2023-07-30 20:29:31 +0700

    [REFACTOR] CONFIG FIREBASE

--- src/Api/firebase.js
+++ src/Api/firebase.js
@@ -3,22 +3,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+// CONFIG FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyA8ZyVOHrJiWdDuZp7g1IS9TmSfyYaD_XY",
-  authDomain: "social-network-68ab1.firebaseapp.com",
-  projectId: "social-network-68ab1",
-  storageBucket: "social-network-68ab1.appspot.com",
-  messagingSenderId: "690156407214",
-  appId: "1:690156407214:web:b6bcec2aa8d653a339e03f",
-  measurementId: "G-P8RD7SV68D",
-  // apiKey: "AIzaSyC8yWcLoFfOWK4hBDDnGLTIoAjhy4a_qxw",
-  // authDomain: "social-network-826a9.firebaseapp.com",
-  // databaseURL: "https://social-network-826a9-default-rtdb.firebaseio.com",
-  // projectId: "social-network-826a9",
-  // storageBucket: "social-network-826a9.appspot.com",
-  // messagingSenderId: "795485769634",
-  // appId: "1:795485769634:web:10fcfc3870e39037842cfb",
-  // measurementId: "G-MDVM9Q2D5H"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID",
 };
 
 const app = initializeApp(firebaseConfig);
